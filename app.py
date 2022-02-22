f762e1e5b842aac11528f6a88838fb7d64c66ebc
Maya Kumar <mayakumar@Mayas-MacBook-Pro-4.local>
2022-02-21 22:53:11 -0800

    finished module.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import scraping
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:22017/mars_app"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 @app.route("/")
@@ -21,4 +21,5 @@ def scrape():
     return redirect('/', code=302)
 
 if __name__ == "__main__":
+    app.debug = True 
     app.run()   
